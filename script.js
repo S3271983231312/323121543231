df7fab047fb2bbb04707aa281ea27a3c85747f13
S3271983231312 <ecologico.silva@tutamail.com>
2026-04-03 19:27:09 -0300

    Update catalog URLs - 2026-04-03 19:27:09

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const CONFIG = {
         'byxatab': 'https://9f293758.byxatab.pages.dev',
         'dodi': 'https://ab408277.dodi.pages.dev',
         'ecologica': 'https://d9a6c21a.ecologica2verde.pages.dev',
-        'fitgirl': 'https://1ecc3b7d.ecofitgirl.pages.dev',
+        'fitgirl': 'https://e4b0b78a.ecofitgirl.pages.dev',
         'gog': 'https://cbe8979c.freepcgoggames.pages.dev',
         'onlinefix': 'https://79163d70.onlinefixme.pages.dev',
         'insaneramzes': 'https://52bdd756.insaneramzes.pages.dev'
