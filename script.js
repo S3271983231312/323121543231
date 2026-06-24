3064bb75f2c54e8d6666584cdc936ac1b6dc1765
S3271983231312 <ecologico.silva@tutamail.com>
2026-06-24 18:07:06 -0300

    Update catalog URLs - 2026-06-24 18:07:06

--- script.js
+++ script.js
@@ -131,10 +131,10 @@ const CONFIG = {
     },
     
     sourceUrls: {
-        'byxatab': 'https://6067b5c9.byxatab.pages.dev',
+        'byxatab': 'https://cc836c46.byxatab.pages.dev',
         'dodi': 'https://a345eceb.dodi.pages.dev',
-        'ecologica': 'https://9c9d8243.ecologica2verde.pages.dev',
-        'fitgirl': 'https://7fd3cca1.ecofitgirl.pages.dev',
+        'ecologica': 'https://01a6b9ef.ecologica2verde.pages.dev',
+        'fitgirl': 'https://a2cb5a95.ecofitgirl.pages.dev',
         'gog': 'https://3b60b3cf.freepcgoggames.pages.dev',
         'onlinefix': 'https://32e97753.onlinefixme.pages.dev',
         'insaneramzes': 'https://4ff04c40.insaneramzes.pages.dev'
