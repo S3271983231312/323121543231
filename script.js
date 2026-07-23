c482a4845781ff73151ce1b54707c1233c420b62
S3271983231312 <ecologico.silva@tutamail.com>
2026-07-23 17:44:25 -0300

    Update catalog URLs - 2026-07-23 17:44:27

--- script.js
+++ script.js
@@ -131,12 +131,12 @@ const CONFIG = {
     },
     
     sourceUrls: {
-        'byxatab': 'https://a4faa93e.byxatab.pages.dev',
+        'byxatab': 'https://a873e2a8.byxatab.pages.dev',
         'dodi': 'https://060309a1.dodi.pages.dev',
-        'ecologica': 'https://a2714479.ecologica2verde.pages.dev',
-        'fitgirl': 'https://53c8513b.ecofitgirl.pages.dev',
+        'ecologica': 'https://05b802dc.ecologica2verde.pages.dev',
+        'fitgirl': 'https://6ae59f02.ecofitgirl.pages.dev',
         'gog': 'https://3b60b3cf.freepcgoggames.pages.dev',
-        'onlinefix': 'https://5871d480.onlinefixme.pages.dev',
+        'onlinefix': 'https://727adb26.onlinefixme.pages.dev',
         'insaneramzes': 'https://29439ec7.insaneramzes.pages.dev'
     },
     
